03a3cc9fcafae5c6476e606bc1f322a2d02c9a55
Joseph Mitchell <JoeTheBro841@gmail.com>
2020-09-10 11:56:58 -0400

    gawk exercise 21

--- ex21.sh
+++ ex21.sh
@@ -4,12 +4,18 @@
 #
 # Problem: Count the number of quotes each person had in the top 100 quote
 # list. Then output the list of names in alphabetical order with the number of quotes each.
-cat ex21.input | awk '{
+cat ex21.input | gawk '{
+
+
 		if (match($0,"-")) {
 			name=substr($0,0,match($0,"-")-1);
-			<insert code>
+			names[name]++;
 		}
 	}
 	END {
-			print <insert code> " had " <insert code> " quotes."
-	}'
+			n = asorti(names,namesi)
+			#print n
+			for (i=1;i<=n;i++){
+				print namesi[i] "had", names[namesi[i]],"quotes."
+			}
+}'
